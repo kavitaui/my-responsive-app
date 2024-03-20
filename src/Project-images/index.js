ae559e0a1b356ac23d97816ea01d0c2ac911bd72
kavitaui <122557323+kavitaui@users.noreply.github.com>
2024-03-20 14:41:46 +0000

    learn how to impliment odometer library

--- src/Project-images/index.js
+++ src/Project-images/index.js
@@ -4,7 +4,7 @@ export {default as banner} from "./banner-app1.png";
 export {default as logo} from "./logo.png";
 export {default as banner2} from "./banner-bg.png";
 export {default as shape2} from "./shape2.png";
-export {default as shape7} from "./shape7.png";
+export {default as shape7} from "./.png";
 export {default as shape10} from "./shape10.png";
 export {default as partner1} from "./partner1.png";
 export {default as partner2} from "./partner2.png";
